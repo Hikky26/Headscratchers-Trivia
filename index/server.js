c192105792b7b5f6b353d5d04749268649023c3d
Hikmah Abodunrin <hikky24.7@gmail.com>
2023-05-21 22:15:20 -0500

    update on the project. not done yet. k.

---
+++ index/server.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Function to decode HTML entities
+// function decodeHTMLEntities(text) {
+//     const element = document.createElement('div');
+//     element.innerHTML = text;
+//     return element.innerText;
+//   }
+  
+//   // Decoding the question and answers
+//   response.question = decodeHTMLEntities(response.question);
+//   response.correct_answer = decodeHTMLEntities(response.correct_answer);
+//   response.incorrect_answers = response.incorrect_answers.map(answer => decodeHTMLEntities(answer));
+  
+//   console.log(response);
